fe6ec03ae31443e92195789741f8b0ad157c1a2a
devendra <devendra.shekhawat@habilelabs.io>
2021-04-28 05:35:59 +0530

    set user image

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { AppBar,Toolbar, Avatar, Button } from '@material-ui/core';
 import React from "react";
 import useStyles from "../../custom-hooks/useStyles";
 import style from "../../assets/style"
-import user from '../assets/images/user.jpg';
+import user from '../../assets/images/user.jpg';
 import { fetchAPI } from "../../services/api";
 
 const Header = (props) => {
